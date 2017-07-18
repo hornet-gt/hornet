a1b1aa3ca972e39a94d558fec9f6123274a628ad
federico.busato@univr.it <federico.busato@univr.it>
2017-07-18 20:33:01 +0200

    windows to linux test filename

---
+++ test/BFSTest++.cu
@@ -0,0 +1,33 @@
+/**
+ * @brief Breadth-first Search Top-Down test program (C++11 Style APIs)
+ * @file
+ */
+#include "Static/BreadthFirstSearch/TopDown++.cuh"
+
+int main(int argc, char* argv[]) {
+    using namespace timer;
+    using namespace custinger;
+    using namespace custinger_alg;
+
+    graph::GraphStd<vid_t, eoff_t> graph;
+    CommandLineParam cmd(graph, argc, argv);
+
+    cuStingerInit custinger_init(graph.nV(), graph.nE(), graph.out_offsets_ptr(),
+                                 graph.out_edges_ptr());
+
+    cuStinger custiger_graph(custinger_init);
+
+    BfsTopDown2 bfs_top_down(custiger_graph);
+    bfs_top_down.set_parameters(graph.max_out_degree_id());
+    Timer<DEVICE> TM;
+    TM.start();
+
+    bfs_top_down.run();
+
+    TM.stop();
+    TM.print("TopDown");
+
+    auto is_correct = bfs_top_down.validate();
+    std::cout << (is_correct ? "\nCorrect <>\n\n" : "\n! Not Correct\n\n");
+    return is_correct;
+}
---
+++ test/CCTest.cu
@@ -0,0 +1,35 @@
+/**
+ * @brief Breadth-first Search Top-Down test program (C++11 Style APIs)
+ * @file
+ */
+#include "Static/ConnectedComponents/CC++.cuh"
+
+int main(int argc, char* argv[]) {
+    using namespace graph;
+    using namespace timer;
+    using namespace custinger;
+    using namespace custinger_alg;
+
+    GraphStd<vid_t, eoff_t> graph(Structure::UNDIRECTED);
+    CommandLineParam(graph, argc, argv);
+
+    cuStingerInit custinger_init(graph.nV(), graph.nE(), graph.out_offsets(),
+                                 graph.out_edges());
+
+    graph.print();
+
+    cuStinger custiger_graph(custinger_init);
+    CC cc_multistep(custiger_graph);
+
+    Timer<DEVICE> TM;
+    TM.start();
+
+    cc_multistep.run();
+
+    TM.stop();
+    TM.print("CC");
+
+    auto is_correct = cc_multistep.validate();
+    std::cout << (is_correct ? "\nCorrect <>\n\n" : "\n! Not Correct\n\n");
+    return is_correct;
+}

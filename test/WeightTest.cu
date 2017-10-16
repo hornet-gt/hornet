fd3840b955162b7b45a98eb9df8889957747fca9
Kasimir Gabert <kasimir@gatech.edu>
2017-10-16 10:02:03 -0400

    Actually adding in test file

---
+++ test/WeightTest.cu
@@ -0,0 +1,16 @@
+#include "Hornet.hpp"
+#include <GraphIO/GraphWeight.hpp>
+
+void exec(int argc, char* argv[]);
+
+/**
+ * @brief Ensure that the GraphIO GraphWeight class can read weights
+ * @author Kasimir Gabert <kasimir@gatech.edu>
+ */
+int main(int argc, char* argv[]) {
+    graph::GraphWeight<int32_t, int32_t, float> graph;
+    graph.read(argv[1]);
+    graph.print();
+
+    return 0;
+}

2910e972456e2d9407697d2d70ec77df087ca667
Kasimir Gabert <kasimir@gatech.edu>
2017-10-16 11:45:31 -0400

    Using more standard vid_t, eoff_t

--- test/WeightTest.cu
+++ test/WeightTest.cu
@@ -1,12 +1,16 @@
 #include "Hornet.hpp"
 #include <GraphIO/GraphWeight.hpp>
 
+using weight_t = float;
+
+using namespace hornets_nest;
+
 /**
  * @brief Ensure that the GraphIO GraphWeight class can read weights
  * @author Kasimir Gabert <kasimir@gatech.edu>
  */
 int main(int argc, char* argv[]) {
-    graph::GraphWeight<int32_t, int32_t, float> graph;
+    graph::GraphWeight<vid_t, eoff_t, weight_t> graph;
     graph.read(argv[1]);
     graph.print();
 

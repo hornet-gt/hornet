7bed811f4fd5e713b67a64ef7264c71b3e2298cf
James <jfox43@gatech.edu>
2017-11-06 01:29:25 -0500

    added two prototype operators for triangle-counting intersections in test file. does not compile

--- test/ForAllAdjIntersectionsTest.cu
+++ test/ForAllAdjIntersectionsTest.cu
@@ -27,6 +27,61 @@ struct GetAttr {
     }
 };
 
+/*
+ * Naive intersection operator
+ * Assumption: access to entire adjacencies of v1 and v2 required
+ */
+struct OPERATOR_AdjIntersectionCount {
+    int* vTriangleCounts;
+
+    OPERATOR(Vertex& v1, Vertex& v2) {
+        int equalCount = 0;
+        int outDeg1 = v1.out_degree();
+        int outDeg2 = v2.out_degree();
+        int eoff1 = 0;
+        int eoff2 = 0;
+        vid_t vid_curr1;
+        vid_t vid_curr2;
+        int comp;
+        while (eoff1 < outDeg1 && eoff2 < outDeg2) {
+            vid_curr1 = v1.edge(eoff1).dst_id();
+            vid_curr2 = v2.edge(eoff2).dst_id();
+            comp = vid_curr1 - vid_curr2;
+            equalCount += (comp == 0);
+            eoff1 += (comp <= 0 && !0);
+            eoff2 += (comp >= 0 && !0);
+        }
+        atomicAdd(vTriangleCounts+v1.id(), equalCount);
+    }
+};
+
+/*
+ * Intersect operator optimized for better work-balance opportunities.
+ * Assumption: operates on subarrays of adjacencies of v1 and v2
+ * @input FLAG: indicates specific information regarding this operation
+ */
+struct OPERATOR_AdjIntersectionCount2 {
+    int* vTriangleCounts;
+
+    OPERATOR(EdgeIt& eit1, vid_t end1, EdgeIt& eit2, vid_t end2, int FLAG) {
+        int equalCount = 0;
+        vid_t vid_curr1;
+        vid_t vid_curr2;
+        int comp;
+        while (*eit1 <= end1 && eit2 <= end2) {
+            vid_curr1 = *eit1.dst_id();
+            vid_curr2 = *eit2.dst_id();
+            comp = vid_curr1 - vid_curr2;
+            equalCount += (comp == 0);
+            if (comp <= 0)
+                eit1++;
+            if (comp <= 0)
+                eit2++;
+        }
+        atomicAdd(vTriangleCounts+v1.id(), equalCount);
+    }
+};
+
 int main(int argc, char* argv[]) {
 
     graph::GraphStd<vid_t, eoff_t> graph;

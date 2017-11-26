e08b1aad778447fc3072edceacfad4b0e7c8b311
FedericoUnivr <federico.busato@univr.it>
2017-11-26 12:33:14 -0800

    copyTest

--- test/Tile/main.cu
+++ test/Tile/main.cu
@@ -47,8 +47,53 @@ void workStructKernel(const T* __restrict__ d_in, int size, T* d_out) {
     d_out[0] = test[size];
 }
 
+template<typename T>
+__global__
+void copyTest1(const T* __restrict__ d_in, int size, T* __restrict__ d_out) {
+    int id     = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = id; i < size; i += stride)
+        d_out[i] = d_in[i];
+}
+
+template<typename T, typename R>
+__global__
+void copyTest2(const T* __restrict__ d_in1,
+               const R* __restrict__ d_in2,
+               int size,
+               T* __restrict__ d_out1,
+               R* __restrict__ d_out2) {
+    int id     = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = id; i < size; i += stride) {
+        d_out1[i] = d_in1[i];
+        d_out2[i] = d_in2[i];
+    }
+}
+
+template<typename T, typename R>
+__global__
+void copyTest3(const T* __restrict__ d_in1,
+               const T* __restrict__ d_in2,
+               const R* __restrict__ d_in3,
+               int size,
+               T* __restrict__ d_out1,
+               T* __restrict__ d_out2,
+               R* __restrict__ d_out3) {
+    int id     = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = id; i < size; i += stride) {
+        d_out1[i] = d_in1[i];
+        d_out2[i] = d_in2[i];
+        d_out3[i] = d_in3[i];
+    }
+}
+
 int main(int argc, char* argv[]) {
-    Timer<DEVICE> TM;
+    /*sTimer<DEVICE> TM;
 
     using T = int;
     const int size = (1 << 29);
@@ -97,5 +142,51 @@ int main(int argc, char* argv[]) {
     workStructKernel
         <<< xlib::ceil_div<BLOCK_SIZE * MUL>(size), BLOCK_SIZE >>>
     //copyKernel <<< xlib::ResidentBlocks<BLOCK_SIZE>::value, BLOCK_SIZE >>>
-        (d_in, size, d_out);
+        (d_in, size, d_out);*/
+
+#define TEST1
+    //__________________________________________________________________________
+    int size = (1 << 30);
+
+#if defined(TEST3)
+    int16_t *d_in1, *d_in2, *d_out1, *d_out2;
+    int8_t *d_in3, *d_out3;
+    cuMalloc(d_in1, size);
+    cuMalloc(d_in2, size);
+    cuMalloc(d_in3, size);
+    cuMalloc(d_out1, size);
+    cuMalloc(d_out2, size);
+    cuMalloc(d_out3, size);
+#elif defined(TEST1)
+    int* d_in4, *d_out4;
+    cuMalloc(d_in4, size);
+    cuMalloc(d_out4, size);
+#elif defined (TEST2)
+    int16_t* d_in5, *d_out5;
+    int8_t *d_in6, *d_out6;
+    cuMalloc(d_in5, size);
+    cuMalloc(d_out5, size);
+    cuMalloc(d_in6, size);
+    cuMalloc(d_out6, size);
+#endif
+
+    Timer<DEVICE> TM;
+
+    for (int i = (1 << 16); i < size; i *= 2) {
+        TM.start();
+
+#if defined(TEST3)
+        copyTest3 <<< xlib::ceil_div<BLOCK_SIZE>(i), BLOCK_SIZE >>>
+            (d_in1, d_in2, d_in3, i, d_out1, d_out2, d_out3);
+#elif defined (TEST2)
+        copyTest2 <<< xlib::ceil_div<BLOCK_SIZE>(i), BLOCK_SIZE >>>
+            (d_in5, d_in6, i, d_out5, d_out6);
+#elif defined(TEST1)
+        copyTest1 <<< xlib::ceil_div<BLOCK_SIZE>(i), BLOCK_SIZE >>>
+            (d_in4, i, d_out4);
+#endif
+
+        TM.stop();
+        std::cout << "i:\t" << i << "\t" << TM.duration() << std::endl;
+    }
 }
